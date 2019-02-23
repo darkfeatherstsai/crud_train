df4c9714ad001a59a0a8d3e1ae5181b463979ec1
John Tsai <darkfeathers_0916@hotmail.com>
2019-02-23 16:37:17 +0800

    fix vote_log_db

--- db/migrate/20190114062341_create_vote_logs.rb
+++ db/migrate/20190114062341_create_vote_logs.rb
@@ -1,7 +1,7 @@
 class CreateVoteLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :vote_logs do |t|
-      t.references :candidare, foreign_key: true
+      t.references :candidate, foreign_key: true
       t.string :ip_address
 
       t.timestamps
